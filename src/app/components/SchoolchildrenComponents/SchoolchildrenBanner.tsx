16b0960cf052b42da1adc2b6ae9456635e4cbb0b
MDR <imdanya946@gmail.com>
2025-03-21 17:18:48 +0200

    done correction

--- src/app/components/SchoolchildrenComponents/SchoolchildrenBanner.tsx
+++ src/app/components/SchoolchildrenComponents/SchoolchildrenBanner.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import "./styles/SchoolchildrenBanner.css";
 import HighlightedTextWithDots from "../HomeComponents/styledComponents/HighlightedTextWithDots";
 import HighlightedText from "../HomeComponents/styledComponents/HighlightedText";
+import { div } from "framer-motion/client";
 export default function SchoolchildrenBanner() {
   const links = [
     {
@@ -26,58 +27,94 @@ export default function SchoolchildrenBanner() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-    const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
-    checkScreenSize();
-    window.addEventListener("resize", checkScreenSize);
-    return () => window.removeEventListener("resize", checkScreenSize);
+      const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
+      checkScreenSize();
+      window.addEventListener("resize", checkScreenSize);
+      return () => window.removeEventListener("resize", checkScreenSize);
     }
   }, []);
   const linksContainer = (
     <div className="schoolchildren-banner-content-text-links">
-          {links.map((link, index) => {
-            return (
-              <div
-                key={index}
-                className="schoolchildren-banner-content-text-link"
-                onClick={(e) => {
-                  e.preventDefault();
-                  const target = document.querySelector(link.href);
-                  target?.scrollIntoView({ behavior: "smooth", block: "start" });
-                }}
-              >
-                {isMobile ? link.textMobile :link.text}
-              </div>
-            );
-          })}
+      {links.map((link, index) => {
+        return (
+          <div
+            key={index}
+            className="schoolchildren-banner-content-text-link"
+            onClick={(e) => {
+              e.preventDefault();
+              const target = document.querySelector(link.href);
+              target?.scrollIntoView({ behavior: "smooth", block: "start" });
+            }}
+          >
+            {isMobile ? link.textMobile : link.text}
+          </div>
+        );
+      })}
+    </div>
+  );
+  const bannerRightText = isMobile ? (
+    <div className="schoolchildren-banner-right-text-container">
+      <div className="schoolchildren-banner-right-text">
+        <div className="schoolchildren-banner-right-text-item">
+          Курс <span>10</span> місяців
         </div>
-  )
-  const bannerRightText = isMobile ? (<div className="schoolchildren-banner-right-text">
-    <div className="schoolchildren-banner-right-text-item">Курс <span>10</span> місяців</div>
-    <div className="schoolchildren-banner-right-text-item">Онлайн/Офлайн заняття</div>
-    <div className="schoolchildren-banner-right-text-item">Одне заняття від <span>230</span> грн</div>
-  </div>) : (<div className="schoolchildren-banner-right-text">
-          school · exams · speak club
-        </div>);
+        <div className="schoolchildren-banner-right-text-item">
+          Онлайн/Офлайн заняття
+        </div>
+        <div className="schoolchildren-banner-right-text-item">
+          Одне заняття від <span>230</span> грн
+        </div>
+      </div>
+      <button
+        className="schoolchildren-banner-right-text-btn"
+        onClick={() => {
+          const target = document.getElementById("price-table");
+          if (target) {
+            setTimeout(() => {
+              target.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+              });
+            }, 100);
+          }
+        }}
+      >
+        Дізнатися більше
+      </button>
+    </div>
+  ) : (
+    <div className="schoolchildren-banner-right-text">
+      school · exams · speak club
+    </div>
+  );
   return (
     <div className="schoolchildren-banner-container">
       <div className="schoolchildren-banner-content">
         <div className="schoolchildren-banner-content-text-header">
-        {isMobile ?<HighlightedText colorBack="#fce696">англійська</HighlightedText> : "англійська"} для {isMobile? null:<br />}
-          {isMobile ? "школярів" :<HighlightedTextWithDots
-            colorText="#ffffff"
-            colorBackground="#BFA0BEB2"
-            colorDots="#AE88AD"
-            widthDots={isMobile ? 8 : 10}
-            widthBorder={isMobile ? 3 : 4}
-          >
-         школярів
-          </HighlightedTextWithDots>}
+          англійська для {isMobile ? null : <br />}
+          {isMobile ? (
+            <HighlightedText colorBack="#fce696">школярів</HighlightedText>
+          ) : (
+            <HighlightedTextWithDots
+              colorText="#ffffff"
+              colorBackground="#BFA0BEB2"
+              colorDots="#AE88AD"
+              widthDots={isMobile ? 8 : 10}
+              widthBorder={isMobile ? 3 : 4}
+            >
+              школярів
+            </HighlightedTextWithDots>
+          )}
         </div>
         {isMobile ? null : linksContainer}
       </div>
       <div className="schoolchildren-banner-right">
         <div className="schoolchildren-banner-right-img">
-          <img loading="lazy"src="/assets/images/forSchoolchildren.png" alt="" />
+          <img
+            loading="lazy"
+            src="/assets/images/forSchoolchildren.png"
+            alt=""
+          />
         </div>
         {!isMobile ? null : linksContainer}
         {bannerRightText}
