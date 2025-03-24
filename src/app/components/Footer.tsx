98f4195266bd79bd420b3afc467ebc032d8a4836
MDR <imdanya946@gmail.com>
2025-03-24 22:58:55 +0200

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,15 @@
-"use client"; 
+"use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
 import "../styles/Footer.css";
 
 export default function Footer({ background }: { background: boolean }) {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-      if (typeof window !== "undefined") {
+    if (typeof window !== "undefined") {
       const updateLayout = () => {
         setIsMobile(window.innerWidth < 768);
       };
@@ -23,9 +24,17 @@ export default function Footer({ background }: { background: boolean }) {
     <div
       className="footer-container"
       style={{
-        backgroundImage: background ? "url('/assets/background/back-footer.png')" : "none",
+        backgroundImage: background
+          ? "url('/assets/background/back-footer.png')"
+          : "none",
         paddingTop: background ? 80 : 0,
-        height: background ? (isMobile ? "auto" : 400) : isMobile ? "auto" : 350,
+        height: background
+          ? isMobile
+            ? "auto"
+            : 400
+          : isMobile
+          ? "auto"
+          : 350,
       }}
     >
       <div className="footer-info">
@@ -34,17 +43,26 @@ export default function Footer({ background }: { background: boolean }) {
             <h1>АДРЕСА</h1>
             <h2>вул. Степана Бандери</h2>
           </div>
-          <div className="footer-info-map">Подивитися на карті</div>
+          <a
+            href="https://maps.app.goo.gl/UPMrS4pAHhR2GVoJ9"
+            className="footer-info-map"
+          >
+            Подивитися на карті
+          </a>
         </div>
         <div className="footer-info-contacts-container">
           <div className="footer-info-contacts">
             <h1>КОНТАКТИ</h1>
-            <h2>+380</h2>
+            <h2>+38 (067) 71 49 862</h2>
             <h2>пошта@gmail.com</h2>
           </div>
           <div className="footer-info-socials">
-            <img src="/assets/instagram-icon.png" alt="Instagram" />
-            <img src="/assets/telegram-icon.png" alt="Telegram"  />
+            <a href="https://www.instagram.com/new_line_school_boryspil/">
+              <FaInstagram className="footer-info-socials-icon" />
+            </a>
+            <a href="https://t.me/new_line_school_boryspil">
+              <FaTelegram className="footer-info-socials-icon" />
+            </a>
           </div>
         </div>
         <div className="footer-info-work-schedule-container">
